416a194be5b6f4a6f87bcc1d11e658b50d95dbd4
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2024-02-17 11:07:48 -0500

    forecast model

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,7 +2,8 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:weatherappg7/models/wather_model.dart';
+import 'package:weatherappg7/models/weather_model.dart';
+import 'package:weatherappg7/services/api_services.dart';
 import 'package:weatherappg7/widgets/forecast_item.dart';
 import 'package:weatherappg7/widgets/weather_item.dart';
 import 'package:http/http.dart' as http;
@@ -14,17 +15,6 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   WeatherModel? _model;
-
-  Future<WeatherModel> getWeatherInfo(double lat, double lang) async {
-    Uri url = Uri.parse(
-        "http://api.weatherapi.com/v1/current.json?key=70866d7ade244a3c9ca20142230509&q=$lat,$lang&aqi=no");
-    http.Response response = await http.get(url);
-    print(response.body);
-    Map<String, dynamic> data = json.decode(response.body);
-    WeatherModel weatherModel = WeatherModel.fromJson(data);
-    print(weatherModel.location.country);
-    return weatherModel;
-  }
 
   Future<Position> getLocation() async {
     bool _serviceEnabled;
@@ -47,12 +37,20 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  Future<void> getDataLocation() async {
+    Position position = await getLocation();
+    _model = await ApiServices()
+        .getWeatherInfo(position.latitude, position.longitude);
+    setState(() {});
+  }
+
   @override
   void initState() {
-    getWeatherInfo(-11.240446, -77.626040).then((value) {
-      _model = value;
-      setState(() {});
-    });
+    // getWeatherInfo(-11.240446, -77.626040).then((value) {
+    //   _model = value;
+    //   setState(() {});
+    // });
+    getDataLocation();
     // TODO: implement initState
     super.initState();
   }
@@ -61,13 +59,13 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xff282B30),
-      floatingActionButton: FloatingActionButton(onPressed: () {
-        getWeatherInfo(-11.240446, -77.626040);
-        // getLocation().then((value) {
-        //   print(value.latitude);
-        //   print(value.longitude);
-        // });
-      }),
+      // floatingActionButton: FloatingActionButton(onPressed: () {
+      //   getWeatherInfo(-11.240446, -77.626040);
+      //   // getLocation().then((value) {
+      //   //   print(value.latitude);
+      //   //   print(value.longitude);
+      //   // });
+      // }),
       appBar: AppBar(
         title: Text("Weather App"),
         centerTitle: true,
--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -1,3 +1,20 @@
-import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class ApiServices {}
+import 'package:http/http.dart' as http;
+import 'package:weatherappg7/models/weather_model.dart';
+
+class ApiServices {
+  Future<WeatherModel?> getWeatherInfo(double lat, double lang) async {
+    Uri url = Uri.parse(
+        "http://api.weatherapi.com/v1/current.json?key=70866d7ade244a3c9ca20142230509&q=$lat,$lang&aqi=no");
+    http.Response response = await http.get(url);
+    print(response.statusCode);
+    print(response.body);
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = json.decode(response.body);
+      WeatherModel weatherModel = WeatherModel.fromJson(data);
+      return weatherModel;
+    }
+    return null;
+  }
+}
