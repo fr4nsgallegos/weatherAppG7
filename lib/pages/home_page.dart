d641285a26c0347880ba834fa98e764457e84670
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2024-02-16 21:54:16 -0500

    permissions geolocator

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class HomePage extends StatelessWidget {
+  Future<Position> getLocation() async {
+    bool _serviceEnabled;
+    LocationPermission _permission;
+    _serviceEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if (!_serviceEnabled) {
+      return Future.error("Servicios de geolocalización estan deshabilitados");
+    }
+
+    _permission = await Geolocator.checkPermission();
+    if (_permission == LocationPermission.denied) {
+      _permission = await Geolocator.requestPermission();
+      if (_permission == LocationPermission.denied) {
+        return Future.error("Los permisos han sido denegados");
+      }
+    }
+    return await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        getLocation().then((value) {
+          print(value.latitude);
+          print(value.longitude);
+        });
+      }),
       appBar: AppBar(),
       body: Column(),
     );
